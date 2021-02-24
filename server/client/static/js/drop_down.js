f6c74f4b7006e2b00aabaa5b6c2e4a70c13405d9
Michael <1mikegrn@gmail.com>
2021-02-24 10:25:05 -0800

    update blog links

--- server/client/static/js/drop_down.js
+++ server/client/static/js/drop_down.js
@@ -7,6 +7,6 @@ Pages
     <a class="dropdown-item" href="https://github.com/1mikegrn">Github Profile</a>
     <div class="dropdown-divider"></div>
     <a class="dropdown-item" href="/portfolio">Portfolio</a>
-    <a class="dropdown-item" href="https://1mikegrn.github.io/blog/">Blog</a>
+    <a class="dropdown-item" href="https://blog.michaelgreen.dev/">Blog</a>
 </div>
 `);
